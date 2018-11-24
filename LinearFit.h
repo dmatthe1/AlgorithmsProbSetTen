c021395af82110fa92bc392ab28242e351fc3a04
Daniel Matthewson <danieltmatthewson@gmail.com>
2018-11-23 21:59:27 -0600

    starter code

--- LinearFit.h
+++ LinearFit.h
@@ -10,7 +10,39 @@
 
 using namespace std;
 
+
+//LUP Decomposition Method
 template<class T,class F>
-vector<double> fitFuncs(const vector<T> &data,const vector<F> &functions) {
-  
+vector<double> fitFuncs(const vector<T> &data, const vector<F> &functions) {
+    val n = data.size();
+    val matrix;
+    vector<T> arr (n);
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = i;
+    }
+
+    for (int k = 0; k < n; k++) {
+        val p = 0;
+        for (int i = k; i < n; i++) {
+            if () {
+
+            }
+        }
+        if (p == 0) {
+            //error singular matrix
+        }
+        //exchange
+
+        for (int i = 0; i < n; i++) {
+            //exchange
+        }
+
+        for (int i = k + 1; i < n; i++) {
+            //equality
+            for (int j = k + 1; j < n; j++) {
+                matrix[i][j] = matrix[i][j] - (matrix[i][k] * matrix[k][j]);
+            }
+        }
+    }
 }
