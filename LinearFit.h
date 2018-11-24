df68d83d12281c50b134f01d436fc8a830b2754d
Daniel Matthewson <dmatthe1@trinity.edu>
2018-11-23 20:47:34 -0600

    Update LinearFit.h

--- LinearFit.h
+++ LinearFit.h
@@ -2,3 +2,15 @@
 //Problem Set Ten: Linear Fit
 //Team F
 //Pledged
+
+#include <math.h>
+#include <vector>
+#include <stdio.h>
+#include<algorithm>
+
+using namespace std;
+
+template<class T,class F>
+vector<double> fitFuncs(const vector<T> &data,const vector<F> &functions) {
+  
+}
