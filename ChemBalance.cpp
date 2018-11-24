c329ff78707685284ab797a92057733f0c32ab8c
Daniel Matthewson <dmatthe1@trinity.edu>
2018-11-23 20:48:00 -0600

    Update ChemBalance.cpp

--- ChemBalance.cpp
+++ ChemBalance.cpp
@@ -2,3 +2,14 @@
 //Problem Set Ten: Chem Balance
 //Team F
 //Pledged
+
+#include <math.h>
+#include <vector>
+#include <stdio.h>
+#include<algorithm>
+
+using namespace std;
+
+tuple<vector<int>,vector<int>> chemBalance(const string &equ) {
+  
+}
