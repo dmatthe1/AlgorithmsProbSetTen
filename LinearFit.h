8f48cbacbced4bd7adcf54d50203b0b4bff7ef93
dmatthe1 <dmatthe1@trinity.edu>
2018-11-24 15:44:52 -0600

    added some documentation

--- LinearFit.h
+++ LinearFit.h
@@ -13,14 +13,17 @@ using namespace std;
 //LUP Decomposition Method
 template<class T,class F>
 vector<double> fitFuncs(const vector<T> &data, const vector<F> &functions) {
-    val n = data.size();
-    val matrix;
-    vector<T> arr (n);
+    val n = data.size(); //size of the data
+    val matrix; //matrix of size n
+    vector<T> arr(n); //array of size n
+    vector<double> returnVect; //vector to return
 
+    //
     for (int i = 0; i < n; i++) {
         arr[i] = i;
     }
 
+    //
     for (int k = 0; k < n; k++) {
         val p = 0;
         for (int i = k; i < n; i++) {
@@ -28,15 +31,18 @@ vector<double> fitFuncs(const vector<T> &data, const vector<F> &functions) {
 
             }
         }
+        //check for singular matrix error
         if (p == 0) {
             //error singular matrix
         }
         //exchange
 
+        //
         for (int i = 0; i < n; i++) {
             //exchange
         }
 
+        //
         for (int i = k + 1; i < n; i++) {
             //equality
             for (int j = k + 1; j < n; j++) {
